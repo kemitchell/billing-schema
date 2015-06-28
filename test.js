e760e892c4b7dcd7f6cebe944568ad220a8a75ec
Kyle Mitchell <kyle@kemitchell.com>
2015-06-28 07:32:04 +0000

    Require estimate amount for estimated projects

--- test.js
+++ test.js
@@ -1,11 +1,23 @@
+var tv4 = require('tv4')
+
 require('tape')(function(test) {
+
   test.deepEqual(
-    require('tv4')
-      .validateMultiple(
-        require('./example'),
-        require('./schema')),
+    tv4.validateMultiple(
+      require('./hourly-example'),
+      require('./schema')),
     { "errors": [],
       "missing": [],
       "valid": true },
-    "validates example")
+    "validates hourly example")
+
+  test.deepEqual(
+    tv4.validateMultiple(
+      require('./estimate-example'),
+      require('./schema')),
+    { "errors": [],
+      "missing": [],
+      "valid": true },
+    "validates estimate example")
+
   test.end() })
