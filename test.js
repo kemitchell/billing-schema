bf22c9dddc80811f544fffc1fb67e27385f5f2dc
Kyle Mitchell <kyle@kemitchell.com>
2015-06-28 16:50:15 +0000

    Use spans of time for entries

--- test.js
+++ test.js
@@ -5,9 +5,9 @@ var schema = require('./schema')
 require('tape')(function(test) {
 
   test.deepEqual(
-    tv4.validateMultiple(require('./hourly-example'), schema),
+    tv4.validateMultiple(require('./time-example'), schema),
     { "errors": [], "missing": [], "valid": true },
-    "validates hourly example")
+    "validates time example")
 
   test.deepEqual(
     tv4.validateMultiple(require('./estimate-example'), schema),
