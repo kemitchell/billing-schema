fe93a7de054be9f5bdcef46c79024526a764a08e
Kyle Mitchell <kyle@kemitchell.com>
2015-06-28 09:27:38 +0000

    Improve test readability

--- test.js
+++ test.js
@@ -1,23 +1,17 @@
 var tv4 = require('tv4')
 
+var schema = require('./schema')
+
 require('tape')(function(test) {
 
   test.deepEqual(
-    tv4.validateMultiple(
-      require('./hourly-example'),
-      require('./schema')),
-    { "errors": [],
-      "missing": [],
-      "valid": true },
+    tv4.validateMultiple(require('./hourly-example'), schema),
+    { "errors": [], "missing": [], "valid": true },
     "validates hourly example")
 
   test.deepEqual(
-    tv4.validateMultiple(
-      require('./estimate-example'),
-      require('./schema')),
-    { "errors": [],
-      "missing": [],
-      "valid": true },
+    tv4.validateMultiple(require('./estimate-example'), schema),
+    { "errors": [], "missing": [], "valid": true },
     "validates estimate example")
 
   test.end() })
